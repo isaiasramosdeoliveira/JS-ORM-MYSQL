f72c41ce0cbfbe5b5c9998eb8678bffd7595cf13
isaiasramosdeoliveira <isaias.ramos.olive@gmail.com>
2023-05-22 15:05:51 -0300

    settings created

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000
+
+app.listen(port, (req, res) =>{
+    console.log(`http://localhost:${port}`);
+})
