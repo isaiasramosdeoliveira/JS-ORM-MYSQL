94de999a0d35094597de8bb46e8d08e0aa0c1680
isaiasramosdeoliveira <isaias.ramos.olive@gmail.com>
2023-05-23 14:32:05 -0300

    finished CRUD

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,14 +1,66 @@
 const database = require("../models");
 
-class PessoaController{
-    static async getAllPeople(req, res){
-        try{
-            const response = await database.Pessoas.findAll();
-            return res.status(200).json(response);
-        } catch(err){
-            return res.status(500).json(err.message);
-        }
+class PessoaController {
+  static async getAllPeople(req, res) {
+    try {
+      const response = await database.Pessoas.findAll();
+      return res.status(200).json(response);
+    } catch (err) {
+      return res.status(500).json(err.message);
     }
+  }
+  static async getPerson(req, res) {
+    try {
+      const { id } = req.params;
+      const response = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(response);
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
+  static async addPerson(req, res) {
+    try {
+      const dice = req.body;
+      const response = await database.Pessoas.create(dice);
+      return res.status(200).json({
+        message: "Pessoa adicionada com sucesso.",
+        response,
+      });
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
+  static async updatePerson(req, res) {
+    try {
+      const { id } = req.params;
+      const dice = req.body;
+      const response = await database.Pessoas.update(dice, {
+        where: { id: Number(id) },
+      });
+      return res.status(200).json({
+        message: "Pessoa atualizada com sucesso.",
+      });
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
+  static async deletePerson(req, res) {
+    try {
+      const { id } = req.params;
+      const response = database.Pessoas.destroy({
+        where: {
+          id: Number(id),
+        },
+      });
+      return res.status(200).json({
+        message: "Pessoa deletada com sucesso.",
+      });
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRouter.js
+++ api/routes/pessoasRouter.js
@@ -3,7 +3,11 @@ const PessoaController = require("../controllers/PessoaController");
 
 const router = express.Router();
 router
-    .get("/pessoas", PessoaController.getAllPeople);
+  .get("/pessoas", PessoaController.getAllPeople)
+  .get("/pessoas/:id", PessoaController.getPerson)
+  .post("/pessoas", PessoaController.addPerson)
+  .put("/pessoas/:id", PessoaController.updatePerson)
+  .delete("/pessoas/:id", PessoaController.deletePerson);
 
 
 module.exports = router;
